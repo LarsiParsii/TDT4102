3f82d24a73e131fd7660edf3c3820ff8e80bd2a7
Lars Fredrik Sveine Onsager <lars-on@hotmail.com>
2023-02-06 02:40:20 +0100

    Finished task 1.

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -16,12 +16,33 @@ int inputInteger(void) {
     return number;
 }
 
+void inputIntegersAndPrintSum(void) {
+    int sum = inputInteger() + inputInteger();
+    cout << "Summen av tallene: " << sum << "\n" << endl;
+}
+
+bool isOdd(int num) {
+    return num % 2 != 0;
+}
+
+void printHumanReadableTime(int seconds) {
+    int hours = seconds / (60*60);    // 1 hour = 60 seconds * 60 minutes
+    int minutes = (seconds % (60*60)) / 60;
+    seconds = seconds % 60;
+    cout << hours << " timer, " << minutes << " minutter, " << seconds << " sekunder\n" << endl;
+}
+
 
 int main() {
     while (run) {
-        cout << "Velg funksjon: \n0) Avslutt \n1) Skriv og print heltall \n" <<
-                "2) Returner heltall \n3) FUNKSJON 3" << endl;
-        cout << "Angi valg (0-3):";
+        cout << "Velg funksjon: \n" <<
+                "0) Avslutt \n" <<
+                "1) Skriv og print heltall \n" <<
+                "2) Returner heltall \n" <<
+                "3) Print sum av input \n" <<
+                "4) Sjekk oddetall \n" << 
+                "5) Sekunder til tekst" << endl;
+        cout << "Angi valg (0-4):";
         cin >> choice;
         cout << endl;
 
@@ -42,9 +63,21 @@ int main() {
             break; }
         case 3:
             // Function 3
+            inputIntegersAndPrintSum();
+            break;
+        case 4:
+            // Function 3
+            for (int num = 0; num <= 15; num++) {
+                cout << (isOdd(num) ? "true" :  "false") << "\n";
+            }
+            cout << endl;
+            break;
+        case 5:
+            // Function 3
+            printHumanReadableTime(inputInteger());
             break;
         default:
-            // Not a valid number retry.
+            // Not a valid number. Retry.
             cout << "Valg ma vaere mellom 0 og 3!\n" << endl;
             break;
         }
@@ -52,3 +85,8 @@ int main() {
     }
     return 0;
 }
+
+/* 
+Oppgave 1e)
+Jeg valgte inputInteger fordi jeg ikke har behov for å printe tallet brukeren akkurat skrev inn, men heller bruke input fra brukeren i en funksjon (addering), og så printe dette i terminalen.
+*/
