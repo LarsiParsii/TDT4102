db9d287872dcd37566f444350dec179089f55594
Lars Fredrik Sveine Onsager <lars-on@hotmail.com>
2023-03-26 01:46:33 +0100

    Started assignment 7

---
+++ Assignment 7/main.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+int main() {
+	return 0;
+}
+
+
+/* TEORIOPPGAVER
+*
+* 1a)   Public: Medlemsfunksjonen er synlig for alle, både innenfor og utenfor klassen.
+*       Protected: Funksjonen kan nås innad i selve klassen, samt for dens subklasser.
+*       Private: Funksjonen er kun synlig for selve klassen, og kan ikke nås utenfra.
+*
+*/
